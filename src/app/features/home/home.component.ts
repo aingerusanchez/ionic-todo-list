af1be3423bc269d183dba7fe8d767e7603eca82a
Aingeru Sanchez <aingerusanchez72@gmail.com>
2023-10-31 22:15:10 +0100

    feat: create home standalone component

---
+++ src/app/features/home/home.component.ts
@@ -0,0 +1,12 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss'],
+  standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class HomeComponent {
+
+}
